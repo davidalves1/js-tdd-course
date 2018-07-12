9ae6d571ba519410deaa6deef35cfbbaf38b15c2
davidalves1 <alves.david@outlook.com>
2018-07-12 08:42:22 -0300

    Update

--- webpack-exercise/src/app.js
+++ webpack-exercise/src/app.js
@@ -1,8 +1,8 @@
-import * as R from 'ramda';
+import { union } from 'ramda';
 
 const a = [1, 2, 2, 3, 4, 4];
 const b = [2, 3, 3, 3, 5, 6, 6];
 
-const c = R.union(a, b);
+const c = union(a, b);
 
 console.warn(c);
