ae189f53841cc440378d1c11c8dd3d201bfa161b
davidalves1 <alves.david@outlook.com>
2019-01-14 09:17:03 -0200

    Build scripts and examples

---
+++ github-wrapper/dist/github-wrapper.umd.js
@@ -0,0 +1,215 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["githubWrapper"] = factory();
+	else
+		root["githubWrapper"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./config/index.js":
+/*!*************************!*\
+  !*** ./config/index.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({
+  GITHUB_URL: 'https://api.github.com'
+});
+
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _user = __webpack_require__(/*! ./user */ "./src/user.js");
+
+var _repos = __webpack_require__(/*! ./repos */ "./src/repos.js");
+
+module.exports = {
+  getUser: _user.getUser,
+  getUserRepos: _repos.getUserRepos
+};
+
+/***/ }),
+
+/***/ "./src/repos.js":
+/*!**********************!*\
+  !*** ./src/repos.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getUserRepos = undefined;
+
+var _config = __webpack_require__(/*! ../config */ "./config/index.js");
+
+var _config2 = _interopRequireDefault(_config);
+
+var _utils = __webpack_require__(/*! ./utils */ "./src/utils.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getUserRepos = exports.getUserRepos = function getUserRepos(username) {
+  return fetch(_config2.default.GITHUB_URL + '/users/' + username + '/repos').then(_utils.toJson);
+};
+
+/***/ }),
+
+/***/ "./src/user.js":
+/*!*********************!*\
+  !*** ./src/user.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getUser = undefined;
+
+var _config = __webpack_require__(/*! ../config */ "./config/index.js");
+
+var _config2 = _interopRequireDefault(_config);
+
+var _utils = __webpack_require__(/*! ./utils */ "./src/utils.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getUser = exports.getUser = function getUser(username) {
+  return fetch(_config2.default.GITHUB_URL + '/users/' + username).then(_utils.toJson);
+};
+
+/***/ }),
+
+/***/ "./src/utils.js":
+/*!**********************!*\
+  !*** ./src/utils.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var toJson = exports.toJson = function toJson(data) {
+  return data.json();
+};
+
+/***/ })
+
+/******/ });
+});
+//# sourceMappingURL=github-wrapper.umd.js.map
--- github-wrapper/webpack.config.js
+++ github-wrapper/webpack.config.js
@@ -1,24 +1,27 @@
-const webpack = require('webpack');
 const nodeENV = process.env.NODE_ENV || 'production';
+const { join } = require('path')
 
 module.exports = {
   mode: nodeENV,
   devtool: 'source-map', // Tipos: cheap-eval-source-map (mais leve, porém meno completo), eval-source-map (meio termo), source-map (mais pesado, porém mais completo)
   entry: {
-    filename: './src/app.js'
+    filename: './src/index'
   },
   output: {
-    path: `${__dirname}/public`,
-    filename: './build.js'
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'githubWrapper'
   },
-  devServer: {
-    contentBase: `${__dirname}/public`,
-    compress: true,
-    port: 8001,
-  },  module: {
+  // devServer: {
+  //   contentBase: `${__dirname}/public`,
+  //   compress: true,
+  //   port: 8001,
+  // },
+  module: {
     rules: [
       {
         test: /\.js$/,
+        include: join(__dirname, 'src'),
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -29,9 +32,4 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { 'NODE_ENV': JSON.stringify(nodeENV) }
-    })
-  ]
 }
